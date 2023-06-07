12910abcfeff562d864810c9376c291aa9579a5e
rx-rashid@outlook.com <rx-rashid@outlook.com>
2023-06-07 18:12:08 +0500

    Refactored

--- main/NMEA-parser.h
+++ main/NMEA-parser.h
@@ -9,7 +9,7 @@
  * 
  * * HOW TO USE:
  * Call the above function.
- * Input parameter is string of NMEA code (starting from $ and ending with *xx)
+ * Input parameter is string of NMEA code (starting from $ and ending with *xx)`
  * It returns a pointer to the array of bucket datatype.
  * See `main.c` for how to read the array or read the README.md file in `main` folder
  * * CAUTION:
@@ -24,35 +24,39 @@
 //------------------------------------------------------------------
 union _shared{
     char *str;
-    int num;
+    int   num;
     float points;
 };
+
 typedef struct {
-    int type;
+    int           type;
     union _shared value;  
 }bucket;
 
+char     *_gps_raw    = 0;
+bucket   *_info       = NULL;
+int      _info_size   = 0;
+int      _parse_index = 0;
 
-char *_gps_raw = 0;
-bucket *_info=NULL; int _info_size=0;
-
-int parse_index = 0;
-
-void print_parsed (bucket _parsed){
-    if(_parsed.type==0) printf("%s",_parsed.value.str);
-    if(_parsed.type==1) printf("%d",_parsed.value.num);
-    if(_parsed.type==2) printf("%f",_parsed.value.points);
-    if(_parsed.type==-1) printf("Empty NMEA parameter");
+void print_parsed (bucket _parsed)
+{
+    if ( _parsed.type == 0 )  printf("%s", _parsed.value.str);
+    if ( _parsed.type == 1 )  printf("%d", _parsed.value.num);
+    if ( _parsed.type == 2 )  printf("%f", _parsed.value.points);
+    if ( _parsed.type == -1 ) printf("Empty NMEA parameter");
     printf("\n");
 }
-bool integrity(char* data){
 
-    int i=0,checksum; unsigned char calc = 0; char element = data[i]; char *slice;
-    if (element=='$'){
-        while(1){
+bool integrity(char* data)
+{
+    int  i=0, checksum;
+    unsigned char calc = 0; 
+    char element = data[i]; 
+    char *slice;
+    if (element == '$' ){
+        while (1){
             i++; element = data[i];
-            if (element =='\0')
-            {
+            if (element =='\0'){
                 printf("Invalid NMEA CODE\n");
                 return false;
             }
@@ -73,17 +77,25 @@ bool integrity(char* data){
         return false;
     }
 }
-char get_char(){
-    parse_index++;
-    return _gps_raw[parse_index-1];
-}
-void putback(){
-    parse_index--;
+
+char get_char()
+{
+    _parse_index++;
+    return _gps_raw[_parse_index-1];
 }
 
-bucket comma_parse(){
-    bucket value; char* str_alloc = 0;
-    char slice[15] =""; int i = 0; int type = 0 ; //0 for string, 1 for int, 2 for float
+void putback()
+{
+    _parse_index--;
+}
+
+bucket comma_parse()
+{
+    bucket value;
+    char* str_alloc = 0;
+    char slice[15] ="";
+    int i = 0;
+    int type = 0 ; //0 for string, 1 for int, 2 for float
     int val = get_char();
     if( val >= '0' && val <= '9' ){
         type = 1;
@@ -117,18 +129,18 @@ bucket comma_parse(){
     return value;
 }
 
-bucket* parse_gps_data(char *data){
+bucket* parse_gps_data(char *data)
+{
     _info_size=0;
     if(!_info){
         free(_info);
         _info=0;
     }
-    
-    bucket end; end.type = -2; //End character for our bucket data
+    bucket end;
+    end.type = -2; //End character for our bucket data
     size_t length = strlen(data);
     _gps_raw = malloc((length+1)*sizeof(char));
     strcpy(_gps_raw, data);
-    
     while(get_char()!='$'){}
     while(get_char()!='*'){
         putback();
@@ -142,7 +154,6 @@ bucket* parse_gps_data(char *data){
         }
         putback();
         _info[_info_size-1] = comma_parse(data);
-        // print_parsed(_info[_info_size-1]);
     }
     _info_size++;
     _info = realloc(_info,_info_size*sizeof(bucket));
