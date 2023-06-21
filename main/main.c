4ed73a717e0b3e1ddd79c707104f7eedefe7f3a8
rx-rashid@outlook.com <rx-rashid@outlook.com>
2023-06-21 23:12:38 +0500

    Unit test take1

--- main/main.c
+++ main/main.c
@@ -6,11 +6,14 @@
 #include "NMEA-parser.h"
 #include "unity.h"
 
-#define C_DEBUG
+// #define C_DEBUG
+
 #define ARRAY_SIZE 5
 void parsing_test(void){
-    const bucket_type_t *expected_val = {String,Float,Integer};
-    const char* data[ARRAY_SIZE] = {
+    const bucket_type_t expected_val[] = 
+    {String,Integer,Float,String,Float,String, Integer, Integer, Float, Float, 
+    String, Float, String, Float, String, Undefined};
+    char* data[ARRAY_SIZE] = {
         "$GPGGA,123519.00,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47",
         "$GPGGA,091530.00,3723.2475,N,12158.3416,W,1,8,1.0,10.0,M,,,,*14",
         "$GPGGA,124652.00,5103.743,N,11400.769,W,1,04,1.5,100.0,M,,,,*2B",
@@ -46,15 +49,5 @@ void app_main(void) //For - ESP32
 {
     UNITY_BEGIN();     
     RUN_TEST(parsing_test);
-    UNITY_END();
-
-    
-
-    //Demo to access individual elements
-    printf("\nlongitude:%f %s, latitude:%f %s",
-    output.data[longitude_f].value.f,
-    output.data[long_hemisphere_s].value.s,
-    output.data[latitude_f].value.f,
-    output.data[lat_hemisphere_s].value.s
-    );
+    UNITY_END();   
 }
